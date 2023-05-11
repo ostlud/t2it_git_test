3d476660c0c71f6afde51ffae18c157f79f221ce
ost_lp <ostlud@gmail.com>
2023-05-11 21:29:09 +0300

    remove files

--- lesson7/7.1.py
+++
@@ -1,7 +0,0 @@
-my_string = input()#'Hello, Python!!'
-fun1 = lambda x: x.lower()
-x = ''.join(list(map(fun1, my_string)))
-print(x)
-fun2 = lambda x: x.upper()
-x = ''.join(list(map(fun2, my_string)))
-print(x)
